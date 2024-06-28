048186405d54274df7a3295175b5aff38b6b5f6b
MacBook Pro 13 <macbookpro13@MacBook-Pro-MacBook.local>
2024-06-28 15:59:47 +0300

    make build

---
+++ index.2afb12b8.js
@@ -0,0 +1,2 @@
+var e="calc(50% - 25px)",t=document.getElementsByTagName("div")[0].getElementsByTagName("img")[0];t.style.position="absolute",t.style.left=e,t.style.top=e;
+//# sourceMappingURL=index.2afb12b8.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const CALC = "calc(50% - 25px)";
+const div = document.getElementsByTagName("div")[0];
+const spider = div.getElementsByTagName("img")[0];
+spider.style.position = "absolute";
+spider.style.left = CALC;
+spider.style.top = CALC;
+
+//# sourceMappingURL=index.f75de5e1.js.map
